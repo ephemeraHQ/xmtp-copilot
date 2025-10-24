4672d3ad7c61f698cc6f0e731fa3f2d3b68566f5
Fabrizio Guespe <fguespe@gmail.com>
2025-10-24 16:26:09 -0300

    Refactor command structure

---
+++ src/channels/xmtp.ts
@@ -0,0 +1,54 @@
+import { Agent,  } from "@xmtp/agent-sdk";
+import { logDetails,   } from "@xmtp/agent-sdk/debug";
+import { createSigner, createUser } from "@xmtp/agent-sdk/user";
+import { fromString } from "uint8arrays/from-string";
+import { getRandomValues } from "node:crypto";
+
+// Load .env file only in local development
+if (process.env.NODE_ENV !== 'production') process.loadEnvFile(".env");
+
+const signer = createSigner(createUser(process.env.XMTP_WALLET_KEY as `0x${string}`));
+const dbEncryptionKey = fromString(process.env.XMTP_DB_ENCRYPTION_KEY as `0x${string}`,"hex");
+const randomEncryptionKey = getRandomValues(new Uint8Array(32))
+const agent = await Agent.create(signer, {
+  env: process.env.XMTP_ENV as "local" | "dev" | "production",
+  dbPath: (inboxId) =>(process.env.RAILWAY_VOLUME_MOUNT_PATH ?? "." )+ `/${process.env.XMTP_ENV}-${inboxId.slice(0, 8)}.db3`,
+  dbEncryptionKey: randomEncryptionKey,
+});
+
+
+agent.on("text", async (ctx) => {
+  if (ctx.isDm()) {
+    const messageContent = ctx.message.content;
+    const senderAddress = await ctx.getSenderAddress();
+    console.log(`Received message: ${messageContent} by ${senderAddress}`);
+    await ctx.sendText("gm");
+  }
+});
+
+agent.on("text", async (ctx) => {
+  if (ctx.isGroup() && ctx.message.content.includes("@gm")) {
+    const senderAddress = await ctx.getSenderAddress();
+    console.log(
+      `Received message in group: ${ctx.message.content} by ${senderAddress}`,
+    );
+    await ctx.sendText("gm");
+  }
+});
+
+agent.on("group", async (ctx) => {
+  console.log(`Received message in group: ${ctx.conversation.id}`);
+});
+
+agent.on("dm", async (ctx) => {
+  console.log(`Received message in group: ${ctx.conversation.id}`);
+});
+
+// 4. Log when we're ready
+agent.on("start", (): void => {
+  logDetails(agent.client)
+});
+
+await agent.start();
+
+
